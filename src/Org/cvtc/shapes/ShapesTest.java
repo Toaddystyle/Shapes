353fd42864a939fd4f4d3a760dcf8d196136caa7
tcondon <tcondon31@yahoo.com>
2018-10-03 14:41:39 -0500

    Final Updates to Shapes

--- src/Org/cvtc/shapes/ShapesTest.java
+++ src/Org/cvtc/shapes/ShapesTest.java
@@ -17,25 +17,25 @@ public class ShapesTest {
 		//Component frame = null;
 		
 		// Todd's Helper code to display current directory path
-		System.out.println("Present Project Directory : "+ System.getProperty("user.dir"));
-
+		//System.out.println("Present Project Directory : "+ System.getProperty("user.dir"));
+		ShapeFactory factory = new ShapeFactory(new MessageBox());
 		
 		//  Setting made up numbers to test and show Cuboid
-		//Cuboid cube1 = new Cuboid(2, 4, 6); - Changed!
-		Renderer cube1 = new Cuboid(2, 4, 6, new MessageBoxSub());
+		//Renderer cube1 = new Cuboid(2, 4, 6, new MessageBoxSub());
+		Renderer cube1 = factory.make(ShapeType.Cuboid);
 		// Setting message to display for Cuboid
 		cube1.render();
 		
 		
 		// Setting numbers to test Cylinder
-		//Cylinder cylinder1 = new Cylinder(2, 4);
-		Renderer cylinder1 = new Cylinder(2, 4, new MessageBoxSub());
+		//Renderer cylinder1 = new Cylinder(2, 4, new MessageBoxSub());
+		Renderer cylinder1 = factory.make(ShapeType.Cylinder);
 		// Setting message to display for Cylinder
 		cylinder1.render();
 		
 		// Setting numbers for Sphere
-		//Sphere sphere1 = new Sphere(4);
-		Renderer sphere1 = new Sphere(4, new MessageBoxSub());
+		//Renderer sphere1 = new Sphere(4, new MessageBoxSub());
+		Renderer sphere1 = factory.make(ShapeType.Sphere);
 		sphere1.render();
 	}
 
